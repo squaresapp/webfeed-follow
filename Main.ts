3058cf70359dc2d9d327878a2bb5a9b4417c2bd0
paulgo <spam@heropaul.com>
2024-01-10 10:31:28 -0500

    Expand API to match spec

--- Main.ts
+++ Main.ts
@@ -2,21 +2,58 @@
 namespace WebfeedFollow
 {
 	/**
-	 * Creates an anchor that when clicked, goes to the installed
-	 * webfeed reader (which at this point has to be Squares), and
-	 * instructs the webfeed reader to follow the specified webfeed
-	 * URLs.
+	 * Creates an empty <a> tag that when clicked, causes a redirection
+	 * to the installed webfeed reader (which at this point has to be
+	 * Squares), and instructs the webfeed reader to follow the specified
+	 * webfeed URLs.
 	 */
 	export function createAnchor(...webfeedUrls: string[])
 	{
-		const anchor = document.createElement("a");
-		anchor.href = getRedirectionUrl(webfeedUrls);
-		anchor.addEventListener("click", () =>
+		return convertAnchor(document.createElement("a"), ...webfeedUrls);
+	}
+	
+	/**
+	 * Converts an existing anchor to a a clickable webfeed-follow link.
+	 * If no webfeedUrls are provided, or the specified anchor does not
+	 * have a data-webfeed-href attribute, the anchor is returned without
+	 * modification.
+	 */
+	export function convertAnchor(anchor: HTMLAnchorElement, ...webfeedUrls: string[])
+	{
+		if (webfeedUrls.length === 0)
+			webfeedUrls.push(anchor.getAttribute(dataAttribute) || "");
+		
+		const href = getRedirectionUrl(webfeedUrls);
+		if (href)
 		{
-			writeClipboard(webfeedUrls.join("\n"));
-		});
+			anchor.href = href;
+			anchor.addEventListener("click", () =>
+			{
+				writeClipboard(webfeedUrls.join("\n"));
+			});
+		}
 		
 		return anchor;
+	}
+	
+	/**
+	 * Converts all anchors on the page with the data-webfeed-href attribute
+	 * into webfeed follow links.
+	 */
+	export function convertAllAnchors()
+	{
+		const q = document.querySelectorAll(`A[${dataAttribute}]`);
+		const anchors = Array.from(q) as HTMLAnchorElement[];
+		
+		for (const anchor of anchors)
+		{
+			const webfeedUrl = toAbsoluteOrNull(anchor.getAttribute(dataAttribute) || "");
+			if (!webfeedUrl)
+				continue;
+			
+			anchor.removeAttribute(dataAttribute);
+			convertAnchor(anchor, webfeedUrl);
+		}
 	}
 	
 	/**
@@ -62,14 +99,34 @@ namespace WebfeedFollow
 	 */
 	const clipboardDataTypes = ["text/uri-list", "text/plain"];
 	
+	const dataAttribute = "data-webfeed-href";
+	
 	/** */
 	function getRedirectionUrl(webfeedUrls: string[])
 	{
 		const query = webfeedUrls
-			.map(s => s.replace(/&/g, "%26"))
+			.map(s => toAbsoluteOrNull(s.replace(/&/g, "%26")))
+			.filter((s): s is string => !!s)
 			.join("&");
 		
 		return redirectionUrl + "?" + query;
+	}
+	
+	/**
+	 * Returns the specified string as a fully-qualified URL, relative
+	 * to the current page, or null if the string is not a valid absolute
+	 * or relative URL.
+	 */
+	function toAbsoluteOrNull(maybeUrl: string)
+	{
+		try
+		{
+			const url = new URL(maybeUrl, window.location.href);
+			return url.toString();
+		}
+		catch (e) { }
+		
+		return null;
 	}
 	
 	/**
@@ -79,4 +136,6 @@ namespace WebfeedFollow
 	 * solution to maximize the probility of the proliferation of webfeeds.
 	 */
 	const redirectionUrl = "https://deeplinks.squaresapp.org/";
+	
+	document.addEventListener("DOMContentLoaded", convertAllAnchors);
 }
